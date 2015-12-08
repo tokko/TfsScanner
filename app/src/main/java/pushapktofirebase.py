3a0c2b141c995ddf809d19303095a27b3f135caa
tokko <andreg@kth.se>
2015-12-08 21:28:37 +0100

    updated push script

--- app/src/main/java/pushapktofirebase.py
+++ app/src/main/java/pushapktofirebase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://burning-fire-7618.firebaseio.com', None)
-firebase.authentication = firebase.Authentication('hggG9AcHbass1X2AcuaRFGRZ8tUvOqBrIdqmDIHo', 'andreg@kth.se', extra={'id' : 123})
-in_file = open(":~/TfsScanner/app/build/outputs/apk/app-debug.apk", "rb") # opening for [r]eading as [b]inary
+import os
+fb = firebase.FirebaseApplication('https://burning-fire-7618.firebaseio.com', 'hggG9AcHbass1X2AcuaRFGRZ8tUvOqBrIdqmDIHo')
+#fb.authentication = firebase.Authentication('hggG9AcHbass1X2AcuaRFGRZ8tUvOqBrIdqmDIHo', 'andreg@kth.se', extra={'id' : 123})
+#os.chdir('~/TfsScanner/app/build/outputs/apk')
+in_file = open('app-debug.apk', 'rb') # opening for [r]eading as [b]inary
 data = in_file.read() # if you only wanted to read 512 bytes, do .read(512)
 in_file.close()
 
 data = {'filename' : 'TfsScanner.apk',  'size' : len(data), 'data' : data}
-firebase.post("/", "apk" data)
+fb.post("/", {"apk":'test'}, {'print':'silent'}, {'X_FANCY_HEADER':'VERY_FANCY'})
