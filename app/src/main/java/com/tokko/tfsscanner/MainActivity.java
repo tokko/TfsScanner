3013eb15e5c80ca16be324fb627a7c27123747f1
tokko <andreg@kth.se>
2015-12-08 22:59:45 +0100

    self-autoupdating maybe?

--- app/src/main/java/com/tokko/tfsscanner/MainActivity.java
+++ app/src/main/java/com/tokko/tfsscanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tokko.tfsscanner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
@@ -7,18 +8,36 @@ import android.hardware.camera2.CameraManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.google.zxing.Result;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import me.dm7.barcodescanner.zxing.ZXingScannerView.ResultHandler;
 
-public class MainActivity extends AppCompatActivity implements ResultHandler{
+public class MainActivity extends AppCompatActivity implements ResultHandler, ChildEventListener {
 
     private static final String USER_PREFIX = "user: ";
     private static final String PBI_ID_PREFIX = "id: ";
@@ -33,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
 
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
+
     }
 
     @Override
@@ -40,6 +60,11 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
         super.onResume();
         scannerView.setResultHandler(this);
         scannerView.startCamera(cameraId);
+
+        Firebase.setAndroidContext(this);
+        Firebase ref = new Firebase("https://burning-fire-7618.firebaseio.com/");
+        Query q = ref.child("/apk").limitToLast(1);
+        q.addChildEventListener(this);
     }
 
     @Override
@@ -63,6 +88,9 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
             scannerView.startCamera(cameraId);
             return true;
         }
+        if(id == R.id.update){
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -84,4 +112,69 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
         }, 500);
     }
 
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        HashMap<String, Object> o = (HashMap<String, Object>) dataSnapshot.getValue();
+        HashMap<String, String> h = (HashMap<String, String>) o.get("apk");
+        new Installer(getApplicationContext()).execute(h);
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        onChildAdded(dataSnapshot, s);
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {    }
+
+    private class Installer extends AsyncTask<HashMap<String, String>, Void, Uri>{
+
+        private Context context;
+
+        public Installer(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Uri doInBackground(HashMap<String, String>... params) {
+            HashMap<String, String> d = params[0];
+            long releaseTime = Long.valueOf(d.get("timestamp"));
+            long thisReleasetime = context.getSharedPreferences("downloader", MODE_PRIVATE).getLong("thisReleaseTime", 0);
+            if(releaseTime < thisReleasetime) return null;
+            String encodedString = d.get("data");
+            byte[] data = Base64.decode(encodedString, Base64.DEFAULT);
+            File file = context.getExternalFilesDir(null);
+            if(file == null) return null;
+            File actualFile = new File(file, d.get("filename"));
+            if(actualFile.exists()) actualFile.delete();
+            try {
+                if(!actualFile.createNewFile()) return null;
+                BufferedOutputStream fw = new BufferedOutputStream(new FileOutputStream(actualFile));
+                fw.write(data);
+                fw.flush();
+                fw.close();
+                return Uri.fromFile(actualFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Uri uri) {
+            if(uri != null){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(uri, "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                context.getSharedPreferences("downloader", MODE_PRIVATE).edit().putLong("thisReleaseTime", System.currentTimeMillis()).apply();
+            }
+        }
+    }
 }
--- app/src/main/java/pushapktofirebase.py
+++ app/src/main/java/pushapktofirebase.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 from firebase import firebase
 import os
-fb = firebase.FirebaseApplication('https://burning-fire-7618.firebaseio.com', 'hggG9AcHbass1X2AcuaRFGRZ8tUvOqBrIdqmDIHo')
+import base64
+import time
+
+fb = firebase.FirebaseApplication('https://burning-fire-7618.firebaseio.com', None)
 #fb.authentication = firebase.Authentication('hggG9AcHbass1X2AcuaRFGRZ8tUvOqBrIdqmDIHo', 'andreg@kth.se', extra={'id' : 123})
 #os.chdir('~/TfsScanner/app/build/outputs/apk')
 in_file = open('app-debug.apk', 'rb') # opening for [r]eading as [b]inary
 data = in_file.read() # if you only wanted to read 512 bytes, do .read(512)
 in_file.close()
+fb.delete('/apk', '')
 
-data = {'filename' : 'TfsScanner.apk',  'size' : len(data), 'data' : data}
-fb.post("/", {"apk":'test'}, {'print':'silent'}, {'X_FANCY_HEADER':'VERY_FANCY'})
+millis = int(round(time.time() * 1000))
+data = {'filename' : 'TfsScanner.apk',  'timestamp' : millis, 'data' : base64.standard_b64encode(data)}
+fb.post("/apk", {"apk":data})
